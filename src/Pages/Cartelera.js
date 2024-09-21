e1f524c2df9bb163884f6669e044007f1999cc40
juanagrether <jgrether@udesa.edu.ar>
2024-09-20 23:13:13 -0300

    Cargar mas en carteleras (debe mostrar más contenidos al usuario con cada interacción)

--- src/Pages/Cartelera.js
+++ src/Pages/Cartelera.js
@@ -8,20 +8,36 @@ class Cartelera extends Component {
     this.state = {
       carteleraMovies: [],
       isLoading: true, 
-      error: null 
+      error: null,
+      pagina: 1,
     };
   }
 
   componentDidMount() {
-    const api = 'https://api.themoviedb.org/3/movie/now_playing?api_key=e6a0d8ba2d9778d0953077400f26f011&language=en-US&page=1';
+    this.fetchMovies(); 
+  }
 
-    // fetch para obtener las películas en cartelera
+  fetchMovies = () => {
+    const { pagina, carteleraMovies } = this.state;
+    const api = `https://api.themoviedb.org/3/movie/now_playing?api_key=e6a0d8ba2d9778d0953077400f26f011&language=en-US&page=${pagina}`;
+
     fetch(api)
       .then((response) => response.json())
       .then((data) => {
         if (data.results) {
-          this.setState({ 
-            carteleraMovies: data.results,
+          const peliculasActual = []; 
+        
+          for (let i = 0; i < carteleraMovies.length; i++) {
+            peliculasActual.push(carteleraMovies[i]);
+          }
+
+          // nuevas películas
+          for (let i = 0; i < data.results.length; i++) {
+            peliculasActual.push(data.results[i]);
+          }
+
+          this.setState({
+            carteleraMovies: peliculasActual, 
             isLoading: false 
           });
         } else {
@@ -40,6 +56,12 @@ class Cartelera extends Component {
       });
   }
 
+  handleVerMas = () => {
+    this.setState({
+      pagina: this.state.pagina + 1 
+    }, this.fetchMovies); 
+  };
+
   render() {
     const { carteleraMovies, isLoading, error } = this.state;
 
@@ -53,7 +75,12 @@ class Cartelera extends Component {
         {error ? (
           <p>{error}</p>
         ) : (
-          <MovieGrid movies={carteleraMovies} />
+          <>
+            <MovieGrid movies={carteleraMovies} />
+            <button onClick={this.handleVerMas} disabled={isLoading}>
+              {isLoading ? <Loading />: 'Ver más'}
+            </button>
+          </>
         )}
       </section>
     );
